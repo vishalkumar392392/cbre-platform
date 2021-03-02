621e8e6a8984c894886e8dd50f2aa28f954e7fc6
vishal palla <vishalpalla27@gmail.com>
2021-03-02 17:48:39 +0530

    merged sidebar and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div>
+    <div className="Header">
+      <Sidebar />
       <Header />
     </div>
   );
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Sidebar.css";
 
 function Sidebar(props) {
-  return <div>Sidebar</div>;
+  return (
+    <div className="wrapper">
+      <div className="sidenav">
+        <h4>CBRE DEVX Platform</h4>
+        <ul>
+          <li className="search">
+            <i class="fas fa-search"></i>Search{" "}
+          </li>
+          <li>
+            <i class="fas fa-home"></i>Home
+          </li>
+          <li>
+            <i class="fas fa-puzzle-piece"></i>Apis
+          </li>
+          <li>
+            <i class="far fa-file-word"></i>Docs
+          </li>
+          <li>
+            <i class="fas fa-plus"></i>Create...
+          </li>
+
+          <li className="tech">
+            <i class="fas fa-thumbtack"></i>Tech Radar
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
